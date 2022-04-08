69aeaafc59c26e0b1589878d4cfbbe335b21a1f0
Eber Martins Rodrigues <eberjoe@gmail.com>
2022-04-08 00:20:38 -0300

    First commit

---
+++ src/main/java/com/eberjoe/motorwind/MotorwindApplication.java
@@ -0,0 +1,13 @@
+package com.eberjoe.motorwind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotorwindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotorwindApplication.class, args);
+	}
+
+}
